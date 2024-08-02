a62b5c0da7ee3151380ff46d218b9c467e60b6ad
Joshua Bergonia <joshuajohnbergonia@gmail.com>
2024-08-03 01:17:43 +0800

    Added admin dashboard UI

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import { getUserInfo } from "../localStorage";
 
 const Header = {
     render: () => {
-        const { name } = getUserInfo(); 
+        const { name, isAdmin } = getUserInfo(); 
         return `
         <div class="brand">
             <a href="/#/">ShoeCorner</a>
@@ -10,6 +10,7 @@ const Header = {
         <div>
             ${name ? `<a href="/#/profile">${name}</a>` : `<a href="/#/signin">Sign-In</a>`}
             <a href="/#/cart">Cart</a>
+            ${isAdmin? `<a href="/#/dashboard">Dashboard</a>` : ''}
         </div>`;
     },
     after_render: () => {},
