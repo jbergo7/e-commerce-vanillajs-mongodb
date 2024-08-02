c679b075e2f1321f873bec2a7d45ac1e0fdf5e64
Joshua Bergonia <joshuajohnbergonia@gmail.com>
2024-08-02 09:08:40 +0800

    Added OrderHistory in User's Profile Menu

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,4 +1,4 @@
-import { updateUser } from "../api";
+import { getMyOrders, updateUser } from "../api";
 import { getUserInfo, setUserInfo, clearUser } from "../localStorage";
 import { hideLoading, showLoading, showMessage } from '../utils';
 
@@ -28,39 +28,78 @@ const ProfileScreen = {
         }
       });
   },
-  render: () => {
+  render: async () => {
     const {name, email} = getUserInfo();
     if (!name) {
       document.location.hash = '/';
     }
+    const orders = await getMyOrders();
     return `
-        <div class="form-container">
-         <form id="profile-form">
-          <ul class="form-items">
-          <li>
-            <h1>User Profile</h1>
-          </li>
-          <li>
-            <label for="name">Name</label>
-            <input type="text" name="name" id="name" value="${name}"/>
-          </li>
-          <li>
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" value="${email}"/>
-          </li>
-          <li>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password"/>
-          </li>
-          <li>
-            <Button type="submit" class="primary">Update</button>
-          </li>
-          <li>
-            <Button type="button" id="signout-button" class="">Logout</button>
-          </li>
-          </ul>
-         </form>
-        </div>
+      <div class="content profile">
+       <div class="profile-info">
+       <div class="form-container">
+       <form id="profile-form">
+        <ul class="form-items">
+        <li>
+          <h1>User Profile</h1>
+        </li>
+        <li>
+          <label for="name">Name</label>
+          <input type="text" name="name" id="name" value="${name}"/>
+        </li>
+        <li>
+          <label for="email">Email</label>
+          <input type="email" name="email" id="email" value="${email}"/>
+        </li>
+        <li>
+          <label for="password">Password</label>
+          <input type="password" name="password" id="password"/>
+        </li>
+        <li>
+          <Button type="submit" class="primary">Update</button>
+        </li>
+        <li>
+          <Button type="button" id="signout-button" class="">Logout</button>
+        </li>
+        </ul>
+       </form>
+      </div>
+       </div>
+       <div class="profile-orders">
+       <h2>Order History</h2>
+        <table>
+          <thead>
+          <tr>
+            <th>ORDER ID</th>
+            <th>DATE</th>
+            <th>TOTAL</th>
+            <th>PAID</th>
+            <th>DELIVERED</th>
+            <th>ACTIONS</th>
+          </tr>
+          </thead>
+          <tbody>
+            ${orders.length === 0 ? `<tr><td colspan="6">No Order Found.</tr>` : orders.map(order => `
+              <tr>
+                <td>${order._id}</td>
+                <td>${order.createdAt}</td>
+                <td>${order.totalPrice}</td>
+                <td>${order.paidAt || 'No'}  </td>
+                <td>${order.deliveryAt || 'No'}</td>
+                <td><a href="#/order/${order._id}">DETAILS</a></td>
+              </tr>
+            `).join('\n')
+          
+          }
+          </tbody>
+        </table>
+
+
+       </div>
+      </div>
+
+
+        
         `;
   }
 };
